7c594f10ec982d5f0e7df1d28c9ed612cd654e00
Olabamipe Taiwo <teeola48@gmail.com>
2019-07-25 18:37:41 +0000

    Initial Files setUp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './img/logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div class="row">
+          <h2>Gitfinder Working</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
